09ac3ba41cb6728c0849ae67962844e4f07fd3d1
Francisco Henares <franhenares@MacBook-Air-de-Francisco.local>
2026-04-20 15:40:08 +0200

    Refactor: unificación de estilos de color (withValues) en weekly_plan_page

--- lib/features/weekly_plan/presentation/pages/weekly_plan_page.dart
+++ lib/features/weekly_plan/presentation/pages/weekly_plan_page.dart
@@ -415,7 +415,7 @@ class _SectionCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(16),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.06),
+            color: Colors.black.withValues(alpha: 0.06),
             blurRadius: 12,
             offset: const Offset(0, 4),
           ),
@@ -477,7 +477,7 @@ class _MealCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(14),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.05),
+            color: Colors.black.withValues(alpha: 0.05),
             blurRadius: 8,
             offset: const Offset(0, 2),
           ),
